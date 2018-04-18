93b50cd46ceb89c849a995820156a98801abfe43
Lisa Klein <lisa@q42.nl>
2018-04-18 15:42:33 +0200

    ?

--- AlbertMayhem/Assets/Scripts/ShoppingCart.cs
+++ AlbertMayhem/Assets/Scripts/ShoppingCart.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShoppingCart : MonoBehaviour {
+public class ShoppingCart : MonoBehaviour
+{
+	public float speed = 3;
+	private Rigidbody body;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		body = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		body.AddForce(x, 0, z);
 	}
 }
