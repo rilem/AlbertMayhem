eae9fdacbbcb6725a75ede3be8bf0fe1c79658bd
Lisa Klein <lisa@q42.nl>
2018-04-18 16:14:58 +0200

    shoppingcartcontroller

--- AlbertMayhem/Assets/Scripts/ShoppingCartController.cs
+++ AlbertMayhem/Assets/Scripts/ShoppingCartController.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ShoppingCart : MonoBehaviour
+public class ShoppingCartController : MonoBehaviour
 {
 	public float speed;
 	private Rigidbody body;
 
-	// Use this for initialization
 	void Start ()
 	{
 		body = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {		
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * (speed / 10);
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
